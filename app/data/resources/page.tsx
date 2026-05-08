9078f7ba0bb080a7bfda00dc087186395f8db4d3
Fulocriendy <3528834159@qq.com>
2026-05-08 18:58:57 +0800

    feat: Add app/data/resources/page.tsx

---
+++ app/data/resources/page.tsx
@@ -0,0 +1,10 @@
+import { ResourceLibrary } from '@/components/data'
+
+export default function ResourceLibraryPage() {
+  return <ResourceLibrary />
+}
+
+export const metadata = {
+  title: '科普资源库 | 科普漫步',
+  description: '探索天文、古生物、植物、生态、社区五大领域科普资源',
+}
